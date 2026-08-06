cfec48317c65d0070b87dd828bd646adec6e98ca
Chris Panici <cpanici@gotoccs.com>
2026-08-06 21:29:10 +0000

    Add JobGrid tests and expand test discovery

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,4 +5,8 @@ PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$PROJECT_ROOT"
 
 python manage.py test \
+    accounts \
+    jobgrid \
+    reports \
+    timesheets \
     --settings=ccs_time_reporting.test_settings
